d45f3b8e14218e814309c9a55a7fe1f0d28b571e
Ivan Zelentsov <vanya.zelenczov@mail.ru>
2024-03-06 00:25:42 +0300

    добавление forwardRef

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,37 +1,36 @@
-/* eslint-disable indent */
 import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
 import { useEffect, useReducer, useRef } from 'react';
 import cn from 'classnames';
 import { INITIAL_STATE, formReducer } from './JournalForm.state';
+import Input from '../Input/Input';
 
 function JournalForm({ onSubmit }) {
 	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
 	const { isValid, isFormReadyToSubmit, values } = formState;
-    const titleRef = useRef();
-    const dateRef = useRef();
-    const postRef = useRef();
+	const titleRef = useRef();
+	const dateRef = useRef();
+	const postRef = useRef();
 
-    const focusError = (isValid) => {
-        switch(true) {
-            case !isValid.title:
-                titleRef.current.focus();
-                break;
-            case !isValid.date:
-                dateRef.current.focus();
-                break;
-            case !isValid.post:
-                postRef.current.focus();
-                break;
-        }
-    };
+	const focusError = (isValid) => {
+		switch(true) {
+		case !isValid.title:
+			titleRef.current.focus();
+			break;
+		case !isValid.date:
+			dateRef.current.focus();
+			break;
+		case !isValid.post:
+			postRef.current.focus();
+			break;
+		}
+	};
 
 	useEffect(() => {
 		let timerId;
 		if (!isValid.date || !isValid.post || !isValid.title) {
-            focusError(isValid);
+			focusError(isValid);
 			timerId = setTimeout(() => {
-				console.log('Очистка состояния');
 				dispatchForm({ type: 'RESET_VALIDITY' });
 			}, 2000);
 		}
@@ -43,13 +42,13 @@ function JournalForm({ onSubmit }) {
 	useEffect(() => {
 		if (isFormReadyToSubmit) {
 			onSubmit(values);
-            dispatchForm({ type: 'CLEAR' });
+			dispatchForm({ type: 'CLEAR' });
 		}
 	}, [isFormReadyToSubmit, values, onSubmit]);
 
-    const onChange = (e) => {
-        dispatchForm({ type: 'SET_VALUE', payload: { [e.target.name]: e.target.value } });
-    };
+	const onChange = (e) => {
+		dispatchForm({ type: 'SET_VALUE', payload: { [e.target.name]: e.target.value }});
+	};
 
 	const addJournalItem = (e) => {
 		e.preventDefault();
@@ -59,28 +58,23 @@ function JournalForm({ onSubmit }) {
 	return (
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
 			<div>
-				<input type='text' ref={titleRef} onChange={onChange} value={values.title} name='title' className={cn(styles['input-title'], {
-					[styles['invalid']]: !isValid.title
-				})}/>
+				<Input type='text' ref={titleRef} onChange={onChange} value={values.title} name='title' isValid={!isValid.title}/>
 			</div>
 			<div className={styles['form-row']}>
 				<label htmlFor="date" className={styles['form-label']}>
 					<img src='/calendar.svg' alt='Иконка календаря'/>
 					<span>Дата</span>
 				</label>
-				<input type='date' ref={dateRef} name='date' onChange={onChange} value={values.date} id="date" className={cn(styles['input'], {
-					[styles['invalid']]: !isValid.date
-				})} />
+				<Input type='date' ref={dateRef} onChange={onChange} name='date' value={values.date} id="date" isValid={!isValid.title}/>
 			</div>
 			<div className={styles['form-row']}>
 				<label htmlFor="tag" className={styles['form-label']}>
 					<img src='/file.svg' alt='Иконка папки'/>
 					<span>Метки</span>
 				</label>
-				<input type='text' id="tag" name='tag' onChange={onChange} value={values.tag} className={styles['input']} />
+				<Input type='text' onChange={onChange} id="tag" value={values.tag} name='tag' />
 			</div>
-			
-			<textarea name="post" ref={postRef} id="" cols="30" rows="10" onChange={onChange} value={values.post} className={cn(styles['input'], {
+			<textarea ref={postRef} name="post" id="" onChange={onChange} value={values.post} cols="30" rows="10" className={cn(styles['input'], {
 				[styles['invalid']]: !isValid.post
 			})}></textarea>
 			<Button text="Сохранить" />
